61d1020e89fcd241f9a38b750f90d70a4c69ac6d
Teemu Pärssinen <teemu.parssinen@wunderkraut.com>
2015-09-26 10:43:20 +0300

    add initial files

---
+++ drupal8.old/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularDrupal8App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularDrupal8App
+ */
+angular.module('angularDrupal8App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ drupal8.old/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularDrupal8App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularDrupal8App
+ */
+angular.module('angularDrupal8App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
